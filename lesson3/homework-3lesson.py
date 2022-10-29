e968d8c656043f69bc33fb46be7deaaa0ca00e93
Tradovskiy <flammen41@gmail.com>
2022-10-30 02:02:35 +0300

    new

---
+++ lesson3/homework-3lesson.py
@@ -0,0 +1,28 @@
+#1 Task
+#name = "Jeremy"
+#day = "Sunday"
+
+#print(f"Good day {name}! {day} is a perfect day to learn some python.")
+
+#2 Task
+#name = "Yaroslav"
+#last_name = "Krasnikov"
+#print(name,last_name, sep=' ')
+
+#3 Task
+a = 15
+b = 45
+sum = a + b
+print('Addition result: {2}'.format(a, b, sum))
+sum = a - b
+print('Subtraction result: {2}'.format(a, b, sum))
+sum = a / b
+print('Division result: {2}'.format(a, b, sum))
+sum = a * b
+print('Multiplication result: {2}'.format(a, b, sum))
+sum = a ** b
+print('Exponent result: {2}'.format(a, b, sum))
+sum = a % b
+print('Modulus result: {2}'.format(a, b, sum))
+sum = a // b
+print('Floor division result: {2}'.format(a, b, sum))
