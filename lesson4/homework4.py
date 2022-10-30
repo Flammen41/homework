3f78df7124a287e89973692e0df41e2fc899903d
Tradovskiy <flammen41@gmail.com>
2022-10-31 00:42:34 +0200

    new

---
+++ lesson4/homework4.py
@@ -0,0 +1,23 @@
+#Task1
+#text = "Great Day for Say"
+#count = 0
+#for i in text:
+#	count = count + 1
+#newtext = text[ 0:2 ] + text [count - 2: count ]
+
+#print(newtext)
+
+#Task2
+
+a = '11234556789'
+len(a)
+print("Your phone number : ", a)
+
+if len(a) == 10 and a.isdigit:
+    print("Is phone number")
+else: len(a) < 10
+print('Its not a phone number')
+
+
+#Task3
+#Task4
