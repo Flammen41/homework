a889a60da1a0bcfa20cce3cb00f2e2c4a88ecbf4
Tradovskiy <flammen41@gmail.com>
2022-11-08 21:09:50 +0200

    new

---
+++ lesson7/homework7.py
@@ -0,0 +1,36 @@
+#Task1
+
+# message = "On a summer day somner smith went simming in the sun and his red skin stung"
+# values=['Number', '']
+# list = message.split()
+# mydict = dict(zip(range(len(message))))
+# print(dict)
+############################################################3
+#task2
+
+#
+# stock = {
+#     "banana": 6,
+#     "apple": 0,
+#     "orange": 32,
+#     "pear": 15
+# }
+# prices = {
+#     "banana": 4,
+#     "apple": 2,
+#     "orange": 1.5,
+#     "pear": 3
+# }
+# total_price = {
+#     key: stock.get(key, 0) * prices.get(key, 0) for key in prices.keys()
+# }
+# print(total_price)
+###################################################################################
+#Task3
+
+
+
+
+#Task4
+days_of_week = (Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday)
+dict = {"days_of_week"}
