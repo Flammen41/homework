48ea28d2b6238f9f9eab0fcad39783789ce9be45
Tradovskiy <flammen41@gmail.com>
2022-11-28 23:21:22 +0200

    new

--- lesson14/homework14.py
+++ lesson14/homework14.py
@@ -1,1 +1,14 @@
-
+#Task1
+def my_print_func():
+    def my_fun(x):
+        print("my_fun was called with the argument")
+        return x
+    return my_fun
+@my_print_func()
+def my_main(x):
+    return x
+my_main("Hello World!")
+print(type(my_main))
+print(type(my_print_func))
+#TAsk2
+#Task3
